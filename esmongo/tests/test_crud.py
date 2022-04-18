78e3739bce65ce697f9f8b3b67f38f8745e028e0
unknown <rakkakeren@gmail.com>
2022-04-18 16:01:28 +0700

    add comment and docs in test_crud

--- esmongo/tests/test_crud.py
+++ esmongo/tests/test_crud.py
@@ -5,7 +5,17 @@ from esmongo.loader import load_dummy_data
 
 
 class TestMongoDB:
-    """Test """
+    """
+    Test MongoDB Create, Read, Update and Delete functionalities.
+    The steps are:
+    1. Initialize data and connection.
+    2. Drop collection for us to have a fresh start.
+    3. Perform CRUD test.
+
+    We count the documents on every test to validate the results
+    """
+
+    # 1. Initialize data and connection.
     dummy = load_dummy_data()
     server = MongoDB(
         host=const.HOST_MONGODB,
@@ -13,14 +23,17 @@ class TestMongoDB:
         document_name=dummy["dummy_index"],
     )
 
-    # Drop collection before start
+    # 2. Drop collection before start.
     server.drop_collections()
 
+    # 3. Test CRUD operations on database server.
     def test_insert_single_data(self):
+        # insert_data() method can recognize single and multiple data
         self.server.insert_data(data=self.dummy["dummy_data_1"])
         assert self.server.count_documents(filters={}) == 1
 
     def test_insert_many_data(self):
+        # insert_data() method can recognize single and multiple data
         self.server.insert_data(
             data=[self.dummy["dummy_data_2"], self.dummy["dummy_data_3"]]
         )
@@ -31,24 +44,31 @@ class TestMongoDB:
         assert len(items) == 3
 
     def test_update_single_data(self):
+        # To update MongoDB documents, use {"$set": query} format.
         update = {"$set": self.dummy["dummy_update"]}
+
+        # For single data, set "how" kwarg to "one"
         self.server.update_data(
-            filters=self.dummy["dummy_filter_update"], update=update
+            filters=self.dummy["dummy_filter_update"], update=update, how="one"
         )
         assert (
             self.server.count_documents(filters=self.dummy["dummy_filter_update"]) == 1
         )
 
     def test_update_many_data(self):
+        # To update MongoDB documents, use {"$set": query} format.
         update = {"$set": self.dummy["dummy_update"]}
+
+        # For multiple data, set "how" kwarg to "many"
         self.server.update_data(
-            filters=self.dummy["dummy_filter_update"], update=update
+            filters=self.dummy["dummy_filter_update"], update=update, how="many"
         )
         assert (
             self.server.count_documents(filters=self.dummy["dummy_filter_update"]) == 0
         )
 
     def test_delete_single_data(self):
+        # For single data, set "how" kwarg to "one"
         self.server.delete_data(self.dummy["dummy_filter_delete_1"], how="one")
         assert (
             self.server.count_documents(filters=self.dummy["dummy_filter_delete_1"])
@@ -56,8 +76,8 @@ class TestMongoDB:
         )
 
     def test_delete_many_data(self):
+        # For multiple data, set "how" kwarg to "many"
         self.server.delete_data(self.dummy["dummy_filter_delete_2"], how="many")
-        print(self.server.search_data())
         assert (
             self.server.count_documents(filters=self.dummy["dummy_filter_delete_2"])
             == 0
@@ -65,15 +85,27 @@ class TestMongoDB:
 
 
 class TestES:
+    """
+    Test ElasticSearch Create, Read, Update and Delete functionalities.
+    The steps are:
+    1. Initialize data and connection.
+    2. Drop index for us to have a fresh start.
+    3. Perform CRUD test.
+
+    We count the documents on every test to validate the results
+    """
+
+    # 1. Initialize data and connection.
     dummy = load_dummy_data()
     server = ES(host=const.HOST_ES, username=const.USER_ES, password=const.PWD_ES)
 
-    # Drop index before start, catch the error when no index is found
+    # 2. Drop index before start, catch the error when no index is found.
     try:
         server.drop_index(index_name=dummy["dummy_index"])
     except NotFoundError:
         print("No index found, continuing the test.")
 
+    # 3. Test CRUD operations on database server.
     def test_insert_single_data(self):
         self.server.insert_data(
             index_name=self.dummy["dummy_index"], data=self.dummy["dummy_data_1"]
@@ -91,11 +123,16 @@ class TestES:
         assert resp["count"] == 3
 
     def test_search_data(self):
+        # Search data on ElasticSearch returns dict-like data with many keys.
+        # Get "hits" key to obtain the documents results
         resp = self.server.search_data(index_name=self.dummy["dummy_index"])
         assert len(resp["hits"]) == 3
 
     def test_update_single_data(self):
+        # To perform match in ElasticSearch, use {"match": query} format.
         query = {"match": self.dummy["dummy_filter_update"]}
+
+        # For single data, set "how" kwarg to "one"
         self.server.update_data(
             index_name=self.dummy["dummy_index"],
             query=query,
@@ -108,7 +145,10 @@ class TestES:
         assert resp["count"] == 1
 
     def test_update_many_data(self):
+        # To perform match in ElasticSearch, use {"match": query} format.
         query = {"match": self.dummy["dummy_filter_update"]}
+
+        # For multiple data, set "how" kwarg to "many"
         self.server.update_data(
             index_name=self.dummy["dummy_index"],
             query=query,
@@ -121,29 +161,28 @@ class TestES:
         assert resp["count"] == 0
 
     def test_delete_single_data(self):
+        # To perform match in ElasticSearch, use {"match": query} format.
         query = {"match": self.dummy["dummy_filter_delete_1"]}
+
+        # For single data, set "how" kwarg to "one"
         self.server.delete_data(
             index_name=self.dummy["dummy_index"],
             query=query,
             how="one",
         )
-        resp = self.server.count_documents(
-            index_name=self.dummy["dummy_index"]
-        )
+        resp = self.server.count_documents(index_name=self.dummy["dummy_index"])
         assert resp["count"] == 2
 
     def test_delete_many_data(self):
+        # To perform match in ElasticSearch, use {"match": query} format.
         query = {"match": self.dummy["dummy_filter_delete_2"]}
+
+        # For multiple data, set "how" kwarg to "many"
         self.server.delete_data(
             index_name=self.dummy["dummy_index"],
             query=query,
             how="many",
         )
         resp = self.server.search_data(index_name=self.dummy["dummy_index"])
-        print(resp)
-        resp = self.server.count_documents(
-            index_name=self.dummy["dummy_index"]
-        )
+        resp = self.server.count_documents(index_name=self.dummy["dummy_index"])
         assert resp["count"] == 0
-
-    
