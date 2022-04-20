00123087170eb79c8972eeef7804a3da52ef28aa
unknown <rakkakeren@gmail.com>
2022-04-20 08:33:56 +0700

    make mongo and es task

--- esmongo/main.py
+++ esmongo/main.py
@@ -2,12 +2,14 @@ import warnings
 warnings.filterwarnings("ignore")
 
 from .performance import DBPerformanceTest
-from .tasks import mongo_client, mongodb_task
+from .tasks import mongo_client, mongodb_tasks, es_client, es_tasks
 
 
 # MongoDB Performance Test
-mongo_test = DBPerformanceTest(client=mongo_client, tasks=mongodb_task)
+mongo_test = DBPerformanceTest(client=mongo_client, tasks=mongodb_tasks)
 mongo_test.start()
 
 
 # ElasticSearch Performance Test
+es_test = DBPerformanceTest(client=es_client, tasks=es_tasks)
+es_test.start()
--- esmongo/tasks.py
+++ esmongo/tasks.py
@@ -13,10 +13,37 @@ mongo_client = MongoDB(host=c.HOST_MONGODB, database_name=INDEX_NAME, document_n
 ## Data Source
 json_data = load_from_csv("esmongo/data/test_data.csv")
 
-## Make some list
-mongodb_task = [
-    Task(operation=mongo_client.insert_data, operation_kwargs=dict(data=json_data))
+## Make list of task
+mongodb_tasks = [
+    ### Insert task
+    Task(operation=mongo_client.insert_data, operation_kwargs=dict(data=json_data)),
+
+    ### Search task
+    Task(operation=mongo_client.search_data, operation_kwargs=dict(filters={"name": "ame"})),
+
+    ### Update task
+    Task(operation=mongo_client.update_data, operation_kwargs=dict(filters={"name": "ame"}, update={"$set": {"name": "ami"}}, how="one")),
+
+    ### Delete task
+    Task(operation=mongo_client.delete_data, operation_kwargs=dict(filters={"name": "ami"}, how="one")),
 ]
 
 
+# ElasticSearch Task
+## Initiate connection
 es_client = ES(host=c.HOST_ES, username=c.USER_ES, password=c.PWD_ES, index_name=INDEX_NAME)
+
+## Make list of task
+es_tasks = [
+    ### Insert task
+    Task(operation=es_client.insert_data, operation_kwargs=dict(data=json_data)),
+
+    ### Search task
+    Task(operation=es_client.search_data, operation_kwargs=dict(filters={"name": "ame"})),
+
+    ### Update task
+    Task(operation=es_client.update_data, operation_kwargs=dict(filters={"name": "ame"}, update={"name": "ami"}, how="one")),
+
+    ### Delete task
+    Task(operation=es_client.delete_data, operation_kwargs=dict(filters={"name": "ami"}, how="one")),
+]
